6673cc748597a3d08b33d9c0141c8c54ddeb3191
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-04 09:48:14 +0530

    counter init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Counter from "../components/Counter";
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <h1 className="text-3xl font-bold text-blue-600">
         Install & Setup Vite + React + Typescript + Tailwind CSS 3
-        <Counter />
       </h1>
     </div>
   );

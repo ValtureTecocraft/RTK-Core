281d8fcdd6389549c4ed332540a81bb5901873ab
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-04 11:17:53 +0530

    counter demo with core RTK

--- demo.js
+++ demo.js
@@ -1,36 +1,75 @@
-//import
-import { createStore } from "redux";
-// const { createStore } = require("redux");
+import toolkit from "@reduxjs/toolkit";
 
-//initialize state
+//InitialState
 const initialState = {
-  bal: 5000,
+  counter: 0,
 };
 
-//reducer function
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "DEPOSITE":
-      return {
-        ...state,
-        bal: state.bal + action.value,
-      };
-    case "WITHDRAW":
-      return {
-        ...state,
-        bal: state.bal - action.value,
-      };
-    default:
-      state;
-  }
-};
+//Create Action
+const increment = toolkit.createAction("INCREMENT");
+const decrement = toolkit.createAction("DECREMENT");
+const resetCounter = toolkit.createAction("RESET");
 
-//store
-const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
+/** Customize Create Action : to pass parameters */
+const incrementBy = toolkit.createAction("INCREMENT_BY", (amount, user) => {
+  return {
+    payload: {
+      amount,
+      user,
+      id: toolkit.nanoid(), //for ramdom string generator
+    },
+  };
 });
 
-//action
-store.dispatch({ type: "DEPOSITE", value: 1000 });
-store.dispatch({ type: "WITHDRAW", value: 4000 });
+// console.log(incrementBy(20, "Smit"));
+
+//Create Reducer
+
+//     Method 1: Bulder callback notation:- (RECOMENDED)
+const counterSlice = toolkit.createReducer(initialState, (builder) => {
+  builder.addCase(increment, (state) => {
+    state.counter += 1;
+  });
+  builder.addCase(decrement, (state) => {
+    state.counter -= 1;
+  });
+  builder.addCase(resetCounter, (state) => {
+    state.counter = 0;
+  });
+  builder.addCase(incrementBy, (state, action) => {
+    state.counter += action.payload.amount;
+  });
+});
+
+//     Method 2: map object notation:-
+const counterSlice2 = toolkit.createAction(initialState, {
+  [increment]: (state) => {
+    state.counter += 1;
+  },
+  [decrement]: (state) => {
+    state.counter -= 1;
+  },
+  [resetCounter]: (state) => {
+    state.counter = 0;
+  },
+  [incrementBy]: (state, action) => {
+    state.counter += action.payload.amount;
+  },
+});
+
+//Store
+const store = toolkit.configureStore({
+  reducer: counterSlice,
+});
+
+//dispatch action
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
+store.dispatch(decrement());
+
+store.dispatch(incrementBy(100, "Smit"));
+
+console.log(store.getState());
